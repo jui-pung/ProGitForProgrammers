d5118fcabbc612fa810f90631aba7e03bb7d14cc
JUI-PENG <jennytseng90@gmail.com>
2024-01-05 20:32:48 +0800

    新增一個介面ADD1

---
+++ ProGitForProgrammers/Add1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProGitForProgrammers
+{
+    internal class Add1
+    {
+        // 新增一個介面
+    }
+}
